85663e11ebea71ad29e9416095ce1ba87b88a063
Classic <Classic@DESKTOP-EAT4OHI>
2024-07-28 00:07:08 +0100

    Add project files.

---
+++ CrystalColour/Plugin.cs
@@ -0,0 +1,58 @@
+﻿using BepInEx;
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Utilla;
+
+namespace CrystalColour
+{
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        //"Not a bird"'s Idea
+        GameObject CaveStuff, Crystal;
+        Renderer CrystalRend;
+
+        Color ColToSet => GorillaTagger.Instance.offlineVRRig.playerColor;
+
+        bool caveLoaded;
+        public Plugin()
+        {
+            SceneManager.sceneLoaded += OnSceneLoaded;
+            SceneManager.sceneUnloaded += OnSceneUnLoaded;
+            //"Not a bird"'s Idea
+        }
+
+        private void OnSceneUnLoaded(Scene scene)
+        {
+            if (scene.name == "Cave")
+            {
+                caveLoaded = false;
+                //"Not a bird"'s Idea
+            }
+        }
+        void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            if (scene.name == "Cave")
+            {
+                CaveStuff = GameObject.Find("Cave_Main_Prefab");
+                Crystal = CaveStuff.transform.FindChildRecursive("C_DarkCrystal_Big").gameObject;
+                CrystalRend = Crystal.GetComponent<Renderer>();
+                caveLoaded = true;
+                //"Not a bird"'s Idea
+            }
+        }
+
+        void FixedUpdate()
+        {
+            if (caveLoaded)
+            {
+                if (CrystalRend.material.color != ColToSet)
+                {
+                    CrystalRend.material.color = ColToSet;
+                    //"Not a bird"'s Idea
+                }
+            }
+        }
+    }
+}
---
+++ CrystalColour/PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace CrystalColour
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.graze.gorillatag.crystalcolour";
+        public const string Name = "CrystalColour";
+        public const string Version = "1.0.0";
+    }
+}
